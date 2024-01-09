860b3b7426526a888ef45fcc0743c90a3a5b0050
Geoffrey Picron <geoffrey.picron@arboratum.com>
2024-01-09 11:16:42 +0100

    upgrade

--- solardb/db/data.py
+++ solardb/db/data.py
@@ -585,7 +585,7 @@ class SolarDBData(object):
                 summary_col = idf["summary"]
                 idf.drop(columns="summary", inplace=True)
                 for col in idf:
-                    if idf[col].dtype != np.dtype("timedelta64[ns]"):
+                    if idf[col].dtype != np.dtype("datetime64[ns]"):
                         idf[col] = pd.to_numeric(idf[col], errors='coerce')
 
                 idf = idf.interpolate(method="time")
