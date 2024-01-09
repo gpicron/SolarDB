f9b2e3089102523af6e34a458ef5cbadeb909330
Geoffrey Picron <geoffrey.picron@arboratum.com>
2024-01-09 15:57:50 +0100

    upgrade

--- solardb/db/data.py
+++ solardb/db/data.py
@@ -321,8 +321,7 @@ class SolarDBData(object):
             data=self.get_pp_info(pp_id=pp_id)
         )
 
-        result.drop(columns="interval", inplace=True, errors="ignore")
-        result['start'], result['end'] = result.apply(lambda x: self.get_pp_interval((x.pp_id, x.inv_id), axis=1))
+        result['interval'] = result.apply(lambda x: self.get_pp_interval((x.pp_id, x.inv_id)), axis=1)
         result.set_index([ "pp_id", "inv_id" ], inplace= True)
 
         return result
